e76593e22cf6009835ba3afc5d53533de0ea9f07
alex <picusalex@yahoo.fr>
2017-09-12 23:42:20 +0200

    Correct error during installation process

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SERVICE="system.monitor.service"
 SERVICE_ORG="./$SERVICE"
 SERVICE_DST="/etc/init.d/$SERVICE"
 
-if [ ! -e SERVICE_ORG ]; then
+if [ ! -e $SERVICE_ORG ]; then
     echo "ERROR: file $SERVICE_ORG not found !"
     exit 1
 fi
@@ -15,9 +15,9 @@ fi
 echo "Installing service $SERVICE"
 chmod 0755 $SERVICE_ORG
 if [ -e SERVICE_DST ]; then
-    sudo unlink SERVICE_DST
+    sudo unlink $SERVICE_DST
 fi
-sudo ln -s -v SERVICE_ORG SERVICE_DST
+sudo ln -s -v $SERVICE_ORG $SERVICE_DST
 
 echo "Enable service $SERVICE at boot"
 sudo update-rc.d $SERVICE defaults
